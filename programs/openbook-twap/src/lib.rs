b913885db1b079e8a9549c658871fc95fde75536
metaproph3t <metaproph3t@protonmail.com>
2023-10-12 00:00:00 +0000

    Initial commit

---
+++ programs/openbook-twap/src/lib.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+use openbook_v2::cpi;
+
+declare_id!("EgYfg4KUAbXP4UfTrsauxvs75QFf28b3MVEV8qFUGBRh");
+
+#[account]
+pub struct 
+
+#[program]
+pub mod openbook_twap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+
+    // create market
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
