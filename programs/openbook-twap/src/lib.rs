1f15a5d3b1bd84e8428406fe1860516cc733b08e
metaproph3t <metaproph3t@protonmail.com>
2023-10-13 00:00:00 +0000

    Setup openbook v2 client

--- programs/openbook-twap/src/lib.rs
+++ programs/openbook-twap/src/lib.rs
@@ -3,8 +3,8 @@ use openbook_v2::cpi;
 
 declare_id!("EgYfg4KUAbXP4UfTrsauxvs75QFf28b3MVEV8qFUGBRh");
 
-#[account]
-pub struct 
+//#[account]
+//pub struct 
 
 #[program]
 pub mod openbook_twap {
--- tests/openbook-twap.ts
+++ tests/openbook-twap.ts
@@ -1,16 +1,25 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { OpenbookTwap } from "../target/types/openbook_twap";
+import { OpenBookV2Client } from "@openbook-dex/openbook-v2";
+
+const { PublicKey, Signer, Keypair, SystemProgram } = anchor.web3;
+const { BN, Program } = anchor;
+
+const OPENBOOK_PROGRAM_ID = new PublicKey("8qkavBpvoHVYkmPhu6QRpXRX39Kcop9uMXvZorBAz43o");
+
+export type OpenBookProgram = Program<OpenBookIDL>;
 
 describe("openbook-twap", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.OpenbookTwap as Program<OpenbookTwap>;
+  const openbookTwap = anchor.workspace.OpenbookTwap as Program<OpenbookTwap>;
+  const openbook = new OpenBookV2Client(OPENBOOK_PROGRAM_ID, provider);
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    //await openbook.createMarket(provider.wallet.payer);
+    const tx = await openbookTwap.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
 });
