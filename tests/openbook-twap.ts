94be50f6c7e4a511976e0106db5bbe65e47231ba
metaproph3t <metaproph3t@protonmail.com>
2023-10-13 00:00:00 +0000

    Revert "Complete setting up bankrun"
    This reverts commit beffe7af5afea677aeeed5e997f3415866124daa.

--- tests/openbook-twap.ts
+++ tests/openbook-twap.ts
@@ -5,22 +5,14 @@ import { OpenbookTwap } from "../target/types/openbook_twap";
 import { BankrunProvider } from "anchor-bankrun";
 import { startAnchor, Clock } from "solana-bankrun";
 
-const { PublicKey, Signer, Keypair, SystemProgram } = anchor.web3;
-const { BN, Program } = anchor;
-
-import * as OpenbookTWAPIDL from "../target/idl/openbook_twap.json";
-
-const OPENBOOK_TWAP_PROGRAM_ID = new PublicKey(
-  "EgYfg4KUAbXP4UfTrsauxvs75QFf28b3MVEV8qFUGBRh"
-);
-
 describe("openbook-twap", async function () {
   let provider,
     connection,
     payer,
     context,
-    banksClient,
-    openbookTwap;
+    banksClient;
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
 
   //const program = anchor.workspace.OpenbookTwap as Program<OpenbookTwap>;
   before(async function () {
@@ -28,17 +20,11 @@ describe("openbook-twap", async function () {
     banksClient = context.banksClient;
     provider = new BankrunProvider(context);
     anchor.setProvider(provider);
-
-    openbookTwap = new Program<AutocratProgram>(
-      OpenbookTWAPIDL,
-      OPENBOOK_TWAP_PROGRAM_ID,
-      provider
-    );
   });
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await openbookTwap.methods.createTwapMarket().rpc();
+    const tx = await program.methods.createTwapMarket().rpc();
     console.log("Your transaction signature", tx);
   });
 });
